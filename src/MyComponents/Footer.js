f776695fd774645b9da9bc329d16d50d250ee7f7
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2023-12-30 14:54:29 +0530

    Add files via upload

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-light">
+      <p className="text-center py-3">
+      Copyright &copy; MyToDosList.com
+      </p>
+    </footer>
+  )
+}
+
+// export default Footer
